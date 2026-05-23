3a8e61e3e16fa62914a6b22f9a3bc72774d5aa9d
Rushil <rushilrmalode@gmail.com>
2026-05-23 16:18:34 +0530

    Added changes to preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -4,11 +4,15 @@ import numpy as np
 
 def z_score_normalization(df: pd.DataFrame) -> pd.DataFrame: # This is called typehints, interesting
     df=df.copy()
+    means =[]
+    stds = []
     for col in df.columns[:-1]:
         mean = np.mean(df[col])
         std = np.std(df[col])
+        means.append(mean)
+        stds.append(std)
         df[col] = (df[col]-mean)/std # The normalization z= (x-mu)/sigma ~ N(0,1)
-    return df
+    return df,means,stds
 
 def train_test_split(df: pd.DataFrame,split_ratio: float) -> tuple[pd.DataFrame,pd.DataFrame]:
     sample_size = int(len(df)*split_ratio)
